fd3addab438c45b4ec547eeadb126ef2b7f78cd6
finntoohey <finn_toohey@icloud.com>
2024-05-30 08:55:04 +1000

    Update bossenemy.py

--- bossenemy.py
+++ bossenemy.py
@@ -2,20 +2,20 @@ import pygame
 import game_globals as GAME
 
 class BossEnemy(pygame.sprite.Sprite):
-
+ 
     def __init__(self, x, y):
         super().__init__()
         self.image = pygame.image.load("images/baddietank.png")
         self.image = pygame.transform.scale(self.image, (200, 200))
         self.rect = self.image.get_rect()
         self.rect.center = (x, y)
+        self.health = 5
 
-        self.base_speed = 0.2
-        self.base_spawn_time = 4000
+        self.base_speed = 1
+        self.base_spawn_time = 1500
         self.direction = 1
         self.speed = self.base_speed
         self.spawn_time = self.base_spawn_time
-        self.hit_count = 0
 
     def draw(self, screen):
         screen.blit(self.image, self.rect)
@@ -25,20 +25,14 @@ class BossEnemy(pygame.sprite.Sprite):
 
         screen_rect = pygame.Rect((0, 0), GAME.SCREEN.get_size())
         if GAME.is_sprite_outside_rectangle(self, screen_rect, align=True):
-            self.direction *= -1
+            self.direction *= 0
+            self.rect.y += 0
+            self.speed += 0
 
-        if self.rect.y >= 550:
+        if self.rect.y == 550:
             self.kill()
             GAME.STATE = 'Game Over'
 
     def increase_difficulty(self, level):
-        self.speed = self.base_speed + 0.05 * level
-        self.spawn_time = max(self.base_spawn_time - 50 * level, 2000)
-
-    def hit(self):
-        self.hit_count += 1
-        if self.hit_count >= 5:
-            self.kill()
-            if len(GAME.BOSS_ENEMY_GROUP) < 3:
-                boss_enemy = BossEnemy(512, 100)
-                GAME.BOSS_ENEMY_GROUP.add(boss_enemy)
+        self.speed = self.base_speed + 0.1 * level
+        self.spawn_time = max(self.base_spawn_time - 200 * level, 500)
